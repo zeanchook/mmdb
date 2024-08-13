847c43ee01aa1103605f6d9f325fe97b2db71903
zeanchook50 <zhiyanchook@gmail.com>
2024-08-13 14:58:37 +0800

    refactoring: using redux for updating recent search

--- src/reducer/rootReducer.js
+++ src/reducer/rootReducer.js
@@ -1,40 +1,38 @@
-import { getrecentsearch } from "../service/getrecentsearch-at-service"
+import { getrecentsearch } from "../service/getrecentsearch-at-service";
 
 const result = await getrecentsearch();
-console.log("result",result);
+console.log("result", result);
 
 const initState = {
-    result: result ? result : []
+  result: result ? result : [],
+};
 
-  }
-  
-  
-  
-  const rootReducer = (state = initState, action) => {
-    console.log(state)
-    console.log(action)
+export const rootReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "UPDATE_POST": {
+      const item = action.item;
 
-    if(action.type === "UPDATE_POST")
-    {
-        const item = action.item;
-        const {recentSearch, searchString} = item;
-
-        const newData2 = recentSearch.sort((a, b) => (a.fields.Name-b.fields.Name));
-        let index = newData2.findIndex((item) => item.fields.Name === "1");
-        let existIdx = newData2.findIndex(
-          (item) => item.fields.SearchName === searchString
-        );
-        if (existIdx === -1) {
-          if (index !== -1) {
-            for (let i = newData2.length - 1; i > 0; i--) {
-              newData2[i].fields.SearchName = newData2[i - 1].fields.SearchName;
-            }
-            newData2[index].fields.SearchName = searchString;
+      const { result } = state;
+      const newData2 = result.records.sort(
+        (a, b) => a.fields.Name - b.fields.Name
+      );
+      let index = newData2.findIndex((item) => item.fields.Name === "1");
+      let existIdx = newData2.findIndex(
+        (item) => item.fields.SearchName === item
+      );
+      if (existIdx === -1) {
+        if (index !== -1) {
+          for (let i = newData2.length - 1; i > 0; i--) {
+            newData2[i].fields.SearchName = newData2[i - 1].fields.SearchName;
           }
+          newData2[index].fields.SearchName = item;
         }
-        return {...state,result:{records: newData2}};
+      }
+      return { ...state, result: { records: newData2 } };
     }
-    return state;
+    default:
+      return state;
   }
-  
-  export default rootReducer
+};
+
+export default rootReducer;
