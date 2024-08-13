42e0a45182cbf75d27df9a1f3b26e1d9bd9d5ba7
zeanchook50 <zhiyanchook@gmail.com>
2024-08-13 16:20:31 +0800

    feat: added top level await in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    supported: {
-      'top-level-await': true //browsers can handle top-level-await features
-    },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  },
+  build: {
+    target: 'esnext'
   }
 })
